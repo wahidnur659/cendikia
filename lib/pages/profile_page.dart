257ec92151af17ec0075f1099ebc66668a145d33
Wahid Nurrohim <168250439+wahidnur659@users.noreply.github.com>
2024-06-30 01:05:09 +0700

    tambah icon

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,20 +1,173 @@
-import 'package:cendikia/pages/setting_page.dart';
+import 'package:cendikia/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:cendikia/pages/membership_page.dart';
+import 'package:translator_plus/translator_plus.dart';
 
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends StatefulWidget {
+  @override
+  _AccountScreenState createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  final GoogleTranslator _translator = GoogleTranslator();
+  String _selectedLanguage = 'id'; // Default language code, e.g., 'id' for Indonesian
+
   void _onItemTapped(BuildContext context, String title) {
     switch (title) {
-      case 'Settings':
+      case 'Membership':
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => SettingsPage()),
+          MaterialPageRoute(builder: (context) => MembershipScreen()),
         );
         break;
+      case 'Bahasa':
+        // Implementasi pilihan bahasa (menggunakan showDialog)
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Center(
+              child: AlertDialog(
+                title: Text('Pilih Bahasa'),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          _changeLanguage('id');
+                          Navigator.pop(context); // Tutup dialog setelah memilih bahasa
+                        },
+                        child: Text('Bahasa Indonesia'),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _changeLanguage('en');
+                          Navigator.pop(context); // Tutup dialog setelah memilih bahasa
+                        },
+                        child: Text('Bahasa Inggris'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+        break;
+      case 'Tentang Aplikasi':
+        _showAboutAPKDialog(context);
+        break;
       default:
-        print('$title tapped');
+        print('$title ditekan');
     }
   }
 
+  Future<void> _translateText(BuildContext context, String text, String toLanguageCode) async {
+    try {
+      final Translation translation = await _translator.translate(text, to: toLanguageCode);
+      print('Original: $text\nTranslated: ${translation.text}');
+
+      // Contoh penggunaan hasil terjemahan:
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('${translation.text}'),
+        ),
+      );
+
+      // Simpan preferensi bahasa yang dipilih
+      setState(() {
+        _selectedLanguage = toLanguageCode;
+      });
+
+      // Tampilkan notifikasi pergantian bahasa
+      _showLanguageChangedNotification(context);
+    } catch (error) {
+      print('Error terjemahkan teks: $error');
+    }
+  }
+
+  void _changeLanguage(String languageCode) {
+    // Lakukan terjemahan dan perubahan bahasa di sini
+    _translateText(context, 'Hello, how are you?', languageCode);
+    // Anda bisa menambahkan fungsi untuk merubah semua tampilan bahasa di aplikasi
+    // Contoh: updateLanguageInApp(languageCode);
+  }
+
+  void _showLanguageChangedNotification(BuildContext context) {
+    // Tampilkan notifikasi pergantian bahasa (misalnya, dengan SnackBar)
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Bahasa diubah ke ${_selectedLanguage == 'id' ? 'Bahasa Indonesia' : 'Bahasa Inggris'}'),
+      ),
+    );
+  }
+
+  void _showAboutAPKDialog(BuildContext context) {
+    // Show dialog with APK details
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/cendikia.png', width: 72, height: 72),
+                SizedBox(height: 12),
+                Text('Cendikia', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+              ],
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Versi Aplikasi : 2.0',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Developer : Team Rover',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Tutup'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+    void _handleSignOut(BuildContext context) {
+    // Navigate back to LoginPage
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+      (route) => false, // Prevent going back to the previous page
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +182,7 @@ class AccountScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: 60), // Space for the status bar
+                SizedBox(height: 60), // Spasi untuk status bar
                 CircleAvatar(
                   radius: 50,
                   backgroundImage: AssetImage('assets/ic_profile.png'),
@@ -50,29 +203,37 @@ class AccountScreen extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
-                ListTile(
-                  leading: Icon(Icons.book),
-                  title: Text('Rak Buku'),
+                GestureDetector(
+                  onTap: () => _onItemTapped(context, 'Membership'),
+                  child: ListTile(
+                    leading: Icon(Icons.card_membership),
+                    title: Text('Membership'),
+                  ),
                 ),
-                ListTile(
-                  leading: Icon(Icons.card_membership),
-                  title: Text('Membership'),
+                GestureDetector(
+                  onTap: () => _onItemTapped(context, 'Bahasa'),
+                  child: ListTile(
+                    leading: Icon(Icons.language),
+                    title: Text('Bahasa'),
+                  ),
                 ),
                 ListTile(
                   leading: Icon(Icons.contact_phone),
-                  title: Text('Contact Us'),
+                  title: Text('Hubungi Kami'),
                 ),
                 ListTile(
                   leading: Icon(Icons.rate_review),
                   title: Text('Beri Ulasan'),
                 ),
                 ListTile(
-                  leading: Icon(Icons.store_mall_directory_outlined),
-                  title: Text('Direktori'),
+                  leading: Icon(Icons.adb_outlined),
+                  title: Text('Tentang Aplikasi'),
+                  onTap: () => _onItemTapped(context, 'Tentang Aplikasi'),
                 ),
                 ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
+                  leading: Icon(Icons.logout),
+                  title: Text('Log Out'),
+                  onTap: () => _handleSignOut(context),
                 ),
               ],
             ),
@@ -81,4 +242,10 @@ class AccountScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: AccountScreen(),
+  ));
 }
