92ed554bf7facd506fd8c515723884ca0f4bf0dc
Wahid Nurrohim <168250439+wahidnur659@users.noreply.github.com>
2024-06-30 16:38:38 +0700

    last part

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
       create: (context) => SearchProvider(),
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
