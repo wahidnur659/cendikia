d7edebdf6155d8a33365e27386b8025639784594
Wahid Nurrohim <168250439+wahidnur659@users.noreply.github.com>
2024-06-30 10:44:28 +0700

    finishing

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:cendikia/pages/profile_page.dart';
 import 'package:cendikia/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(
